3c87fe58e6cca1dbfcfe4321c5ff0d7c3edfe5ca
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2021-07-31 22:37:22 +0530

    added another git command to script2.js

--- script2.js
+++ script2.js
@@ -5,4 +5,5 @@ console.log('we use "git commit" to commit the file into repo');
 console.log('we use "git branch" to tell which branch we are on');
 console.log('we use "git diff" to tell us the difference between the new saved file and earlier commited file');
 console.log('we use "git checkout -b (branch-name)" to checkout from current branch and to the new branch');
+console.log('if we want to switch from one branch to another already created branch we use "git checkout (branchname)"');
 
