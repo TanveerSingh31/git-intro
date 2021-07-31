29076e1b7cef208346b62ed295fcb89a594d021d
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2021-07-31 22:30:07 +0530

    added some more info to script2.js

--- script2.js
+++ script2.js
@@ -3,3 +3,6 @@ console.log('we use "git status" to check status');
 console.log('we use "git log" to check history');
 console.log('we use "git commit" to commit the file into repo');
 console.log('we use "git branch" to tell which branch we are on');
+console.log('we use "git diff" to tell us the difference between the new saved file and earlier commited file');
+console.log('we use "git checkout -b (branch-name)" to checkout from current branch and to the new branch');
+
