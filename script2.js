1081ceeafc95559d873b161cbec552e8d5a8269c
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2021-07-31 22:26:12 +0530

    added more info. abt git to script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,5 @@
 console.log('script2.js');
+console.log('we use "git status" to check status');
+console.log('we use "git log" to check history');
+console.log('we use "git commit" to commit the file into repo');
+console.log('we use "git branch" to tell which branch we are on');
